b456a85356fd5c876b03f8c9f0e2255082ef6b2d
Yuvraj arora <86776467+yuvrajarora1805@users.noreply.github.com>
2026-07-21 13:00:54 +0530

    Allow DB override for Docker

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,7 +5,7 @@ export const workermanagePool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: 'workermanage',
+    database: process.env.DB_DATABASE_WORKERMANAGE || 'workermanage',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -16,7 +16,7 @@ export const tempassignmentsPool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: 'tempassignments_db',
+    database: process.env.DB_DATABASE_TEMP || 'tempassignments_db',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
